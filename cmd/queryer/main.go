bfc1de20ace8574f5a957df19f993a23cc9ba2a0
L4B0MB4 <metalfan65@gmail.com>
2024-11-30 00:46:21 +0100

    Refactor event polling by removing legacy implementation and updating event repository initialization

--- cmd/queryer/main.go
+++ cmd/queryer/main.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/L4B0MB4/EVTSRC/pkg/client"
 	"github.com/L4B0MB4/PRYVT/identification/pkg/query/eventhandling"
-	"github.com/L4B0MB4/PRYVT/identification/pkg/query/eventpolling"
 	"github.com/L4B0MB4/PRYVT/identification/pkg/query/httphandler"
 	"github.com/L4B0MB4/PRYVT/identification/pkg/query/httphandler/controller"
 	"github.com/L4B0MB4/PRYVT/identification/pkg/query/store"
 	"github.com/L4B0MB4/PRYVT/identification/pkg/query/store/repository"
 	"github.com/PRYVT/utils/pkg/auth"
+	"github.com/PRYVT/utils/pkg/eventpolling"
+	utilsRepo "github.com/PRYVT/utils/pkg/store/repository"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -36,7 +37,7 @@ func main() {
 		log.Error().Err(err).Msg("Unsuccessful initialization of token manager")
 		return
 	}
-	eventRepo := repository.NewEventRepository(conn)
+	eventRepo := utilsRepo.NewEventRepository(conn)
 	userRepo := repository.NewUserRepository(conn)
 	uc := controller.NewUserController(userRepo, tokenManager)
 	aut := auth.NewAuthMiddleware(tokenManager)
